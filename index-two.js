8b7631a2bd53472c5d968355e126876fae403282
Andrey Zhukov <a.m.zhukov@inbox.ru>
2019-08-29 08:05:53 +0500

    lesson 3, additional task

---
+++ index-two.js
@@ -0,0 +1,28 @@
+
+let lang = prompt("Выберите язык, ru или en. Choose language, ru or en"),
+  daysRu = "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье",
+  daysEn = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday",
+  arr = new Map([
+    ['ru', daysRu],
+    ['en', daysEn]
+  ]),
+  namePerson = prompt("Введите Ваше имя");
+
+if (lang == "ru") console.log(daysRu);
+else if (lang == "en") console.log(daysRu);
+
+switch(lang) {
+  case "ru":
+    console.log(daysRu)
+    break;
+  case "en":
+    console.log(daysEn)
+    break; 
+  }
+ 
+console.log(arr.get(lang));
+
+console.log(namePerson == 'Артём' ? "директор" : (namePerson == 'Максим' ? "преподаватель" : "студент"));
+
+//let result = lang == 'ru' ? console.log(String(days[0].ru)) : '';
+//result = lang == 'en' ? console.log(String(days[0].en)) : '';
